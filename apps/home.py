38fa004ec42e708ef996de0d61db7975cb980951
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-10 19:38:41 +0100

    Update home.py
    updated the storyline

--- apps/home.py
+++ apps/home.py
@@ -6,14 +6,16 @@ def app():
 
     st.title("cycle_safe(berlin) - helping to keep you and your bike safe")
 
-    st.markdown("##### How can we use Machine Learning to mitigate")
-    st.markdown("###### 1. Bike theft risk")
-    st.markdown("###### 2. Bike accident risk")
+    st.write("We offer the following visualized dashboards:")
+    st.write("- Bike Traffic: Bike distribution map gives you an idea about bike traffic throughout the day")    
+    st.write("- Bike Accident: Cluster analysis shows you where the accident hot spots are")    
+    st.write("- Bike Theft: Data analysis and forecasting thefts by districts might save your bike")
 
-    st.image("./images/BikeTheft.png", caption='bike theft')
+    st.image("./images/BikeTheft.png")
 
     st.markdown("### The Team")
-    st.image("./images/group_picture.jpg")
+    st.image("./images/team.jpg")    
+    
     st.markdown("##### Hitoshi Michinaka [Linkedin](https://www.linkedin.com/in/hmichinaka/)")
     st.markdown("##### Dominik Abratanski [Linkedin](https://www.linkedin.com/in/dominikabratanski/)")
     st.markdown("##### Lukas Hartung [Linkedin](https://www.linkedin.com/in/lukas-h-0438578b/)")
