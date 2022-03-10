b9f89bae86be739a0c7dd52533a998166eee0d37
Jakob Hohenstein <jakob-hohenstein@gmx.de>
2022-03-10 16:56:05 +0100

    deleted data sources from theft pages

--- apps/new_app.py
+++ apps/new_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 def app():
     st.title('New App')
     st.write("This is a new app page in the mutliapp.")
+
+    st.image("./images/chrome-capture-2022-2-10.gif", caption='bike theft')
