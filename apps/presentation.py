3d8648fffac0080a1f0135b2d2e82c0d712ec7ea
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-10 14:05:58 +0100

    Update presentation.py

--- apps/presentation.py
+++ apps/presentation.py
@@ -4,9 +4,12 @@ from apps.get_data import read_data, get_geojson, round_up
 import pickle
 import streamlit as st
 import pandas as pd
+import numpy as np
 import geopandas as gpd
 from streamlit_folium import folium_static
-import numpy as np
+from dataclasses import dataclass
+from apps.predict import pred_ts_chart, prediction_by_Bezirk, predict_next_day
+import json
 
 def app():
     
@@ -104,4 +107,64 @@ def app():
     st.write("- Relative ratio of parked NextBike in each area")
     st.write("- 0 = average number of NextBike")
     st.write("Data Source: NextBike live location API")
+    
+#################
+    st.title('3 Thefts - Area')
+    st.write("Total bike theft counts by area between 01.01.2021 and 02.03.2022 (14 months)")
+
+    file = open('./pickle/map_total_theft.pkl', 'rb')
+    object_file = pickle.load(file)
+    file.close()
+    st.plotly_chart(object_file)
+
+    st.write("Data Source: Polizei Berlin")
+##############
+    st.title('3 Thefts - Prediction')
+    st.write("Bike theft prediction / Time Series - Machine Learning model")
+
+#1. Line chart showing last 31 days + predicted number of stolen bikes for tomorrow
+# ---> pred_ts_chart() function from predict.py
+
+    fig = pred_ts_chart()
+    st.plotly_chart(fig)
+    st.write("Data Source: Polizei Berlin")
+
+
+#2. Map showing predicted number of bikes stolen per Bezirk for tomorrow (build it from the provided df)
+# ---> prediction_by_Bezirk() function from predict.py
+
+    pred_date = predict_next_day()["date_reported"][0]
+    st.title(f"Predicted number of stolen bikes on {pred_date:%d.%m.%Y} by Bezirk")
+
+
+    f = open('raw_data/bezirksgrenzen.geojson')
+    geojson_berlin = json.load(f)
+    for k in range(len(geojson_berlin['features'])):
+        geojson_berlin['features'][k]['Bezirk'] = str(geojson_berlin['features'][k]['properties']['Gemeinde_schluessel'])
+
+    df = prediction_by_Bezirk()
+    df.reset_index(inplace=True)
+    df['Prediction_total'] = df['Prediction_total'].apply(lambda x: round(x, 2))
+    df['Bezirk'] = df['Bezirk'].apply(lambda x: '0' + str(x))
+    d = {'001':'Mitte', '002':'Friedrichshain-Kreuzberg', '003':'Pankow',
+                         '004':'Charlottenburg-Wilmersdorf','005':'Spandau','006':'Steglitz-Zehlendorf',
+                         '007':'Tempelhof-Schöneberg','008':'Neukölln', '009':'Treptow-Köpenick',
+                         '010':'Marzahn-Hellersdorf','011':'Lichtenberg','012':'Reinickendorf'}
+    df['Bezirk_name'] = df['Bezirk'].map(d)
+
+    fig = px.choropleth_mapbox(df, geojson=geojson_berlin,
+                               featureidkey='Bezirk', locations='Bezirk',
+                               color='Prediction_total',
+                               range_color = [0, 10],
+                               #hover_name='Bezirk_name',
+                               color_continuous_scale="OrRd",
+                               mapbox_style="open-street-map",
+                               zoom=8.5, opacity=0.8,
+                               center={'lat': 52.52, 'lon': 13.405},
+
+                              )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    st.plotly_chart(fig)
+
+    
     
