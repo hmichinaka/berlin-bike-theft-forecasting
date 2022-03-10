ee4e3c3933b57bb75d4577964cc7ef33f9c78e57
Jakob Hohenstein <23485552+jahlah@users.noreply.github.com>
2022-03-10 18:04:03 +0100

    Update bike_accident1.py

--- apps/bike_accident1.py
+++ apps/bike_accident1.py
@@ -8,17 +8,17 @@ import numpy as np
 
 def app():
     st.title('1. Accidents - Yearly')
-    st.write("Yearly bike accident counts between 01.01.2018 and 31.12.2020 (3 years)")
+    st.markdown("#### Yearly bike accident counts between 01.01.2018 and 31.12.2020 (3 years)")
 
     file = open('./pickle/map_yearly_accident.pkl', 'rb')
     accidents_yearly = pickle.load(file)
     file.close()
     st.plotly_chart(accidents_yearly)
 
-    st.write("Data Source: Amt für Statistik Berlin-Brandenburg")
+    st.markdown("#### Bike accidents in Berlin by year")
     df = pd.DataFrame(np.array(
-        [[8459, 8384, 6701],
-        [5192, 5005, 5109]]),
+        [["8,459", "8,384", "6,701"],
+        ["5,192", "5,005", "5,109"]]),
         columns=['2018', '2019', '2020'])
     df.rename(index={0:'Traffic accident without bike', 1:'Traffic accident with bike'}, inplace=True)
     st.table(df)
