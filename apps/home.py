ee2d244e24fbb4b333ad9d1a3fd1f6445e301996
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-11 14:36:21 +0100

    Update home.py
    removed output_format

--- apps/home.py
+++ apps/home.py
@@ -20,7 +20,7 @@ def app():
     st.markdown("####")
 
     st.markdown("### The Team")
-    st.image("./images/team.jpg", width = 600, output_format='PNG')
+    st.image("./images/team.jpg", width = 600)
 
     st.markdown("##### Hitoshi Michinaka [Linkedin](https://www.linkedin.com/in/hmichinaka/)")
     st.markdown("##### Dominik Abratanski [Linkedin](https://www.linkedin.com/in/dominikabratanski/)")
