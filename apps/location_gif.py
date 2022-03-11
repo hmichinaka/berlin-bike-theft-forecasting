710449d3c85f8a6a339a1e0169047fd74708e0c4
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-11 08:56:47 +0100

    Update location_gif.py
    title added

--- apps/location_gif.py
+++ apps/location_gif.py
@@ -8,6 +8,9 @@ def app():
   data_url = base64.b64encode(contents).decode("utf-8")
   file_.close()
   
+  st.title('Locations - Hourly (GIF)')
+  st.markdown("#### Animated hourly shared bike loactions between 24.02.2022 - 09.03.2022")
+  
   st.markdown(
       f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
       unsafe_allow_html=True,
