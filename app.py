4462071e761b11c9118090fe2b63322f0d439b73
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-11 08:52:19 +0100

    Update app.py
    location_gif is added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats #presentation # import your app modules here
+from apps import home, data_stats, location_gif #presentation # import your app modules here
 from apps import new_app, app_tweets, bike_accident1, bike_accident2, bike_accident3, bike_sharing1, bike_sharing2, bike_theft1, bike_theft2, under_the_hood
 
 app = MultiApp()
@@ -11,6 +11,7 @@ app = MultiApp()
 app.add_app("Home", home.app)
 app.add_app("Locations - Area", bike_sharing1.app)
 app.add_app("Locations - Hourly ", bike_sharing2.app)
+app.add_app("Locations - Hourly (GIF)", location_gif.app)
 app.add_app("Accidents - Yearly", bike_accident1.app)
 app.add_app("Accidents - Hourly", bike_accident2.app)
 app.add_app("Accidents - Clusters", bike_accident3.app)
