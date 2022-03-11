c5fc92b1e11238a04f261ead176f2edc75848964
Paul Roberts <mr.paul.roberts@gmail.com>
2022-03-11 11:18:59 +0100

    Update app.py
    Add Accident EDA to app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import home, data_stats, location_gif #presentation # import your app modules here
 from apps import new_app, app_tweets, bike_accident1, bike_accident2, bike_accident3, bike_sharing1, bike_sharing2, bike_theft1, bike_theft2, under_the_hood
-from apps import accident_gif 
+from apps import accident_gif, accident_EDA
 
 app = MultiApp()
 
@@ -17,7 +17,7 @@ app.add_app("Accidents - Yearly", bike_accident1.app)
 app.add_app("Accidents - Hourly", bike_accident2.app)
 app.add_app("Accidents - Hourly (GIF)", accident_gif.app)
 app.add_app("Accidents - Clusters", bike_accident3.app)
-
+app.add_app("Accidents - EDA", accident_EDA.app)
 app.add_app("Thefts - Facts", bike_theft1.app)
 app.add_app("Thefts - Prediction", bike_theft2.app)
 #app.add_app("4 Under the hood", under_the_hood.app)
