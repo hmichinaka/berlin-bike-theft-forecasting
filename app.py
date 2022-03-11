9920e37b3687bb21d8c89cd425915444cbfe38c8
Hitoshi Michinaka <95750094+hmichinaka@users.noreply.github.com>
2022-03-11 10:08:25 +0100

    Update app.py
    added accidents gif app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats, location_gif #presentation # import your app modules here
+from apps import home, data_stats, location_gif, accident_gif #presentation # import your app modules here
 from apps import new_app, app_tweets, bike_accident1, bike_accident2, bike_accident3, bike_sharing1, bike_sharing2, bike_theft1, bike_theft2, under_the_hood
 
 app = MultiApp()
@@ -14,6 +14,7 @@ app.add_app("Locations - Hourly ", bike_sharing2.app)
 app.add_app("Locations - Hourly (GIF)", location_gif.app)
 app.add_app("Accidents - Yearly", bike_accident1.app)
 app.add_app("Accidents - Hourly", bike_accident2.app)
+app.add_app("Accidents - Hourly (GIF)", accident_gif.app)
 app.add_app("Accidents - Clusters", bike_accident3.app)
 
 app.add_app("Thefts - Facts", bike_theft1.app)
